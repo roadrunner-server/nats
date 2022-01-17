37ea257bf1b46b2bf1a9967fb4383ee975c39f49
Valery Piashchynski <piashchynski.valery@gmail.com>
2022-01-17 17:49:26 +0300

    Initial Commit
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

---
+++ go.mod
@@ -0,0 +1,27 @@
+module github.com/roadrunner-server/nats/v2
+
+go 1.17
+
+require (
+	github.com/json-iterator/go v1.1.12
+	github.com/nats-io/nats.go v1.13.1-0.20211122170419-d7c1d78a50fc
+	github.com/roadrunner-server/api/v2 v2.0.0-rc.2
+	github.com/roadrunner-server/errors v1.1.0
+	github.com/roadrunner-server/sdk/v2 v2.0.0-rc.1
+	go.uber.org/zap v1.20.0
+)
+
+require (
+	github.com/goccy/go-json v0.9.3 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/nats-io/nats-server/v2 v2.7.0 // indirect
+	github.com/nats-io/nkeys v0.3.0 // indirect
+	github.com/nats-io/nuid v1.0.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/roadrunner-server/tcplisten v1.1.0 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.7.0 // indirect
+	golang.org/x/crypto v0.0.0-20220112180741-5e0467b6c7ce // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
---
+++ plugin.go
@@ -0,0 +1,46 @@
+package nats
+
+import (
+	"github.com/roadrunner-server/api/v2/plugins/config"
+	"github.com/roadrunner-server/api/v2/plugins/jobs"
+	"github.com/roadrunner-server/api/v2/plugins/jobs/pipeline"
+	"github.com/roadrunner-server/nats/v2/natsjobs"
+	priorityqueue "github.com/roadrunner-server/sdk/v2/priority_queue"
+	"go.uber.org/zap"
+)
+
+const (
+	pluginName string = "nats"
+)
+
+type Plugin struct {
+	log *zap.Logger
+	cfg config.Configurer
+}
+
+func (p *Plugin) Init(log *zap.Logger, cfg config.Configurer) error {
+	p.log = new(zap.Logger)
+	*p.log = *log
+	p.cfg = cfg
+	return nil
+}
+
+func (p *Plugin) Serve() chan error {
+	return make(chan error, 1)
+}
+
+func (p *Plugin) Stop() error {
+	return nil
+}
+
+func (p *Plugin) Name() string {
+	return pluginName
+}
+
+func (p *Plugin) ConsumerFromConfig(configKey string, queue priorityqueue.Queue) (jobs.Consumer, error) {
+	return natsjobs.FromConfig(configKey, p.log, p.cfg, queue)
+}
+
+func (p *Plugin) ConsumerFromPipeline(pipe *pipeline.Pipeline, queue priorityqueue.Queue) (jobs.Consumer, error) {
+	return natsjobs.FromPipeline(pipe, p.log, p.cfg, queue)
+}
